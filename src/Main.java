8bf6174695abd68868b56b9775e4befedc29318a
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-03-11 09:19:37 +0100

    feat(Main): bucle principal del programa

--- src/Main.java
+++ src/Main.java
@@ -15,17 +15,36 @@ public class Main {
         // levantamos la interfaz
         consola = new InterfazConsola(gestor);
 
+        int opcionElegida;
+        int[] opcionesMenuPrincipal = {0,1,2,3,9};
+
+
+        do {
+            opcionElegida = consola.pedirOpcionMenu(consola.menuPrincipal,opcionesMenuPrincipal);
+
+            switch (opcionElegida) {
+                case 0:
+                    consola.hacerDespedida();
+                    break;
+                case 1:
+                    consola.imprimir(gestor.listarMisiones());
+                    break;
+                case 2:
+                    consola.agregarMision(gestor);
+                    break;
+                case 3:
+                    //todo: buscar mision por id - interfaz
+                    consola.buscarMisionPorId(gestor);
+                    break;
+                case 9:
+                    // eliminarMision
+                    consola.borrarMision(gestor);
+                    break;
+            }
+
+        }while (opcionElegida == 0);
 
     }
-    /*
-    interfaz
-    listar misiones
-    crear mision
-    buscar mision
-    9 eliminar mision (solicita confirmacion
-    0. salir)
-
-     */
 
 }
 
